e27240e42e99c9b28865b8b25fe8dbb3d860201e
pablo-renan17 <pablonichterwistz@outlook.com>
2024-05-15 19:00:15 -0300

    Criação da tela

--- WinFormsApp/Form1.Designer.cs
+++ WinFormsApp/Form1.Designer.cs
@@ -28,12 +28,255 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            spaceForm1 = new ReaLTaiizor.Forms.SpaceForm();
+            spaceLabel3 = new ReaLTaiizor.Controls.SpaceLabel();
+            dreamTextBox3 = new ReaLTaiizor.Controls.DreamTextBox();
+            spaceLabel2 = new ReaLTaiizor.Controls.SpaceLabel();
+            dreamTextBox2 = new ReaLTaiizor.Controls.DreamTextBox();
+            spaceLabel1 = new ReaLTaiizor.Controls.SpaceLabel();
+            dreamTextBox1 = new ReaLTaiizor.Controls.DreamTextBox();
+            spaceBorderLabel1 = new ReaLTaiizor.Controls.SpaceBorderLabel();
+            spaceMaximize1 = new ReaLTaiizor.Controls.SpaceMaximize();
+            spaceClose1 = new ReaLTaiizor.Controls.SpaceClose();
+            spaceMinimize2 = new ReaLTaiizor.Controls.SpaceMinimize();
+            spaceBorderLabel2 = new ReaLTaiizor.Controls.SpaceBorderLabel();
+            spaceForm1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // spaceForm1
+            // 
+            spaceForm1.BackColor = Color.FromArgb(42, 42, 42);
+            spaceForm1.BorderStyle = FormBorderStyle.None;
+            spaceForm1.Controls.Add(spaceBorderLabel2);
+            spaceForm1.Controls.Add(spaceLabel3);
+            spaceForm1.Controls.Add(dreamTextBox3);
+            spaceForm1.Controls.Add(spaceLabel2);
+            spaceForm1.Controls.Add(dreamTextBox2);
+            spaceForm1.Controls.Add(spaceLabel1);
+            spaceForm1.Controls.Add(dreamTextBox1);
+            spaceForm1.Controls.Add(spaceBorderLabel1);
+            spaceForm1.Controls.Add(spaceMaximize1);
+            spaceForm1.Controls.Add(spaceClose1);
+            spaceForm1.Controls.Add(spaceMinimize2);
+            spaceForm1.Customization = "Kioq/yAgIP8qKir/Kioq/xwcHP/+/v7/Kysr/xkZGf8=";
+            spaceForm1.Dock = DockStyle.Fill;
+            spaceForm1.Font = new Font("Verdana", 8F);
+            spaceForm1.Image = null;
+            spaceForm1.Location = new Point(0, 0);
+            spaceForm1.MinimumSize = new Size(200, 25);
+            spaceForm1.Movable = true;
+            spaceForm1.Name = "spaceForm1";
+            spaceForm1.NoRounding = false;
+            spaceForm1.Padding = new Padding(5, 25, 5, 5);
+            spaceForm1.Sizable = true;
+            spaceForm1.Size = new Size(806, 419);
+            spaceForm1.SmartBounds = true;
+            spaceForm1.StartPosition = FormStartPosition.CenterScreen;
+            spaceForm1.TabIndex = 0;
+            spaceForm1.Text = "spaceForm1";
+            spaceForm1.TransparencyKey = Color.Purple;
+            spaceForm1.Transparent = false;
+            // 
+            // spaceLabel3
+            // 
+            spaceLabel3.Customization = "/v7+/yoqKv8=";
+            spaceLabel3.Font = new Font("Verdana", 8F);
+            spaceLabel3.Image = null;
+            spaceLabel3.Location = new Point(96, 202);
+            spaceLabel3.Name = "spaceLabel3";
+            spaceLabel3.NoRounding = false;
+            spaceLabel3.Size = new Size(75, 19);
+            spaceLabel3.TabIndex = 10;
+            spaceLabel3.Text = "Senha:";
+            spaceLabel3.TextAlignment = HorizontalAlignment.Left;
+            spaceLabel3.Transparent = false;
+            // 
+            // dreamTextBox3
+            // 
+            dreamTextBox3.BackColor = Color.FromArgb(41, 41, 41);
+            dreamTextBox3.BorderStyle = BorderStyle.FixedSingle;
+            dreamTextBox3.ColorA = Color.FromArgb(31, 31, 31);
+            dreamTextBox3.ColorB = Color.FromArgb(41, 41, 41);
+            dreamTextBox3.ColorC = Color.FromArgb(51, 51, 51);
+            dreamTextBox3.ColorD = Color.FromArgb(0, 0, 0, 0);
+            dreamTextBox3.ColorE = Color.FromArgb(25, 255, 255, 255);
+            dreamTextBox3.ColorF = Color.Black;
+            dreamTextBox3.ForeColor = Color.FromArgb(40, 218, 255);
+            dreamTextBox3.Location = new Point(96, 227);
+            dreamTextBox3.Name = "dreamTextBox3";
+            dreamTextBox3.Size = new Size(237, 20);
+            dreamTextBox3.TabIndex = 9;
+            // 
+            // spaceLabel2
+            // 
+            spaceLabel2.Customization = "/v7+/yoqKv8=";
+            spaceLabel2.Font = new Font("Verdana", 8F);
+            spaceLabel2.Image = null;
+            spaceLabel2.Location = new Point(96, 146);
+            spaceLabel2.Name = "spaceLabel2";
+            spaceLabel2.NoRounding = false;
+            spaceLabel2.Size = new Size(75, 19);
+            spaceLabel2.TabIndex = 8;
+            spaceLabel2.Text = "Email:";
+            spaceLabel2.TextAlignment = HorizontalAlignment.Left;
+            spaceLabel2.Transparent = false;
+            // 
+            // dreamTextBox2
+            // 
+            dreamTextBox2.BackColor = Color.FromArgb(41, 41, 41);
+            dreamTextBox2.BorderStyle = BorderStyle.FixedSingle;
+            dreamTextBox2.ColorA = Color.FromArgb(31, 31, 31);
+            dreamTextBox2.ColorB = Color.FromArgb(41, 41, 41);
+            dreamTextBox2.ColorC = Color.FromArgb(51, 51, 51);
+            dreamTextBox2.ColorD = Color.FromArgb(0, 0, 0, 0);
+            dreamTextBox2.ColorE = Color.FromArgb(25, 255, 255, 255);
+            dreamTextBox2.ColorF = Color.Black;
+            dreamTextBox2.ForeColor = Color.FromArgb(40, 218, 255);
+            dreamTextBox2.Location = new Point(96, 171);
+            dreamTextBox2.Name = "dreamTextBox2";
+            dreamTextBox2.Size = new Size(237, 20);
+            dreamTextBox2.TabIndex = 7;
+            // 
+            // spaceLabel1
+            // 
+            spaceLabel1.Customization = "/v7+/yoqKv8=";
+            spaceLabel1.Font = new Font("Verdana", 8F);
+            spaceLabel1.Image = null;
+            spaceLabel1.Location = new Point(96, 92);
+            spaceLabel1.Name = "spaceLabel1";
+            spaceLabel1.NoRounding = false;
+            spaceLabel1.Size = new Size(75, 19);
+            spaceLabel1.TabIndex = 6;
+            spaceLabel1.Text = "Nome:";
+            spaceLabel1.TextAlignment = HorizontalAlignment.Left;
+            spaceLabel1.Transparent = false;
+            // 
+            // dreamTextBox1
+            // 
+            dreamTextBox1.BackColor = Color.FromArgb(41, 41, 41);
+            dreamTextBox1.BorderStyle = BorderStyle.FixedSingle;
+            dreamTextBox1.ColorA = Color.FromArgb(31, 31, 31);
+            dreamTextBox1.ColorB = Color.FromArgb(41, 41, 41);
+            dreamTextBox1.ColorC = Color.FromArgb(51, 51, 51);
+            dreamTextBox1.ColorD = Color.FromArgb(0, 0, 0, 0);
+            dreamTextBox1.ColorE = Color.FromArgb(25, 255, 255, 255);
+            dreamTextBox1.ColorF = Color.Black;
+            dreamTextBox1.ForeColor = Color.FromArgb(40, 218, 255);
+            dreamTextBox1.Location = new Point(96, 117);
+            dreamTextBox1.Name = "dreamTextBox1";
+            dreamTextBox1.Size = new Size(237, 20);
+            dreamTextBox1.TabIndex = 5;
+            // 
+            // spaceBorderLabel1
+            // 
+            spaceBorderLabel1.Customization = "Kioq/yoqKv/+/v7/IyMj/yoqKv8=";
+            spaceBorderLabel1.Font = new Font("Verdana", 8F);
+            spaceBorderLabel1.Image = null;
+            spaceBorderLabel1.Location = new Point(96, 286);
+            spaceBorderLabel1.Name = "spaceBorderLabel1";
+            spaceBorderLabel1.NoRounding = false;
+            spaceBorderLabel1.Size = new Size(106, 40);
+            spaceBorderLabel1.TabIndex = 4;
+            spaceBorderLabel1.Text = "Salvar";
+            spaceBorderLabel1.TextAlignment = HorizontalAlignment.Center;
+            spaceBorderLabel1.Transparent = false;
+            // 
+            // spaceMaximize1
+            // 
+            spaceMaximize1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            spaceMaximize1.Customization = "G4qM/3LEtP8yMjL/Kioq/yPJzP8bioz//v7+/yMjI/8qKir/";
+            spaceMaximize1.DefaultAnchor = true;
+            spaceMaximize1.DefaultLocation = true;
+            spaceMaximize1.Font = new Font("Verdana", 8F);
+            spaceMaximize1.Image = null;
+            spaceMaximize1.Location = new Point(756, 3);
+            spaceMaximize1.Name = "spaceMaximize1";
+            spaceMaximize1.NoRounding = false;
+            spaceMaximize1.Size = new Size(23, 21);
+            spaceMaximize1.TabIndex = 3;
+            spaceMaximize1.Text = "+";
+            spaceMaximize1.Transparent = false;
+            spaceMaximize1.WindowState = FormWindowState.Normal;
+            // 
+            // spaceClose1
+            // 
+            spaceClose1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            spaceClose1.Customization = "DQ/S/xhh8/8yMjL/Kioq/x5/9/8ND9L//v7+/yMjI/8qKir/";
+            spaceClose1.DefaultAnchor = true;
+            spaceClose1.DefaultLocation = true;
+            spaceClose1.Font = new Font("Verdana", 8F);
+            spaceClose1.Image = null;
+            spaceClose1.Location = new Point(783, 3);
+            spaceClose1.Name = "spaceClose1";
+            spaceClose1.NoRounding = false;
+            spaceClose1.Size = new Size(23, 21);
+            spaceClose1.TabIndex = 2;
+            spaceClose1.Text = "x";
+            spaceClose1.Transparent = false;
+            // 
+            // spaceMinimize2
+            // 
+            spaceMinimize2.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            spaceMinimize2.Customization = "G4qM/3LEtP8yMjL/Kioq/yPJzP8bioz//v7+/yMjI/8qKir/";
+            spaceMinimize2.DefaultAnchor = true;
+            spaceMinimize2.DefaultLocation = true;
+            spaceMinimize2.Font = new Font("Verdana", 8F);
+            spaceMinimize2.Image = null;
+            spaceMinimize2.Location = new Point(732, 3);
+            spaceMinimize2.Name = "spaceMinimize2";
+            spaceMinimize2.NoRounding = false;
+            spaceMinimize2.Size = new Size(23, 21);
+            spaceMinimize2.TabIndex = 1;
+            spaceMinimize2.Text = "_";
+            spaceMinimize2.Transparent = false;
+            spaceMinimize2.WindowState = FormWindowState.Normal;
+            // 
+            // spaceBorderLabel2
+            // 
+            spaceBorderLabel2.Customization = "Kioq/yoqKv/+/v7/IyMj/yoqKv8=";
+            spaceBorderLabel2.Font = new Font("Verdana", 8F);
+            spaceBorderLabel2.Image = null;
+            spaceBorderLabel2.Location = new Point(233, 286);
+            spaceBorderLabel2.Name = "spaceBorderLabel2";
+            spaceBorderLabel2.NoRounding = false;
+            spaceBorderLabel2.Size = new Size(100, 40);
+            spaceBorderLabel2.TabIndex = 11;
+            spaceBorderLabel2.Text = "Cancelar";
+            spaceBorderLabel2.TextAlignment = HorizontalAlignment.Center;
+            spaceBorderLabel2.Transparent = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(806, 419);
+            Controls.Add(spaceForm1);
+            FormBorderStyle = FormBorderStyle.None;
+            MaximumSize = new Size(1600, 860);
+            MinimumSize = new Size(261, 61);
+            Name = "Form1";
+            ShowIcon = false;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "themeForm1";
+            TransparencyKey = Color.Purple;
+            spaceForm1.ResumeLayout(false);
+            spaceForm1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private ReaLTaiizor.Forms.SpaceForm spaceForm1;
+        private ReaLTaiizor.Controls.SpaceMaximize spaceMaximize1;
+        private ReaLTaiizor.Controls.SpaceClose spaceClose1;
+        private ReaLTaiizor.Controls.SpaceMinimize spaceMinimize2;
+        private ReaLTaiizor.Controls.DreamTextBox dreamTextBox1;
+        private ReaLTaiizor.Controls.SpaceBorderLabel spaceBorderLabel1;
+        private ReaLTaiizor.Controls.SpaceLabel spaceLabel1;
+        private ReaLTaiizor.Controls.SpaceLabel spaceLabel3;
+        private ReaLTaiizor.Controls.DreamTextBox dreamTextBox3;
+        private ReaLTaiizor.Controls.SpaceLabel spaceLabel2;
+        private ReaLTaiizor.Controls.DreamTextBox dreamTextBox2;
+        private ReaLTaiizor.Controls.SpaceBorderLabel spaceBorderLabel2;
     }
 }
---
+++ WinFormsApp/Usuario.cs
@@ -0,0 +1,12 @@
+﻿
+namespace WinFormsApp
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+
+    }
+}
