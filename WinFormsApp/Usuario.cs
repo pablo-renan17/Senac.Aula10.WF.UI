58a343df8f0c6138bd55b19d97cad0e46b67be6c
pablo-renan17 <pablonichterwistz@outlook.com>
2024-05-15 21:52:15 -0300

    adicionado codigos do Suarez, menos o que tem Initialize()

--- WinFormsApp/Usuario.cs
+++ WinFormsApp/Usuario.cs
@@ -1,12 +1,12 @@
-﻿
-namespace WinFormsApp
+﻿namespace WinFormsApp1
 {
     public class Usuario
     {
+        // Id, Nome, Email, Senha
+        //
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Email { get; set; }
         public string Senha { get; set; }
-
     }
 }
