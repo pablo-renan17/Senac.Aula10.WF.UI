062a05e930d2e252c884b1bba7759e0dbc493d62
LoboGamingg <pablorenan12@outlook.com>
2024-05-17 16:25:54 -0300

    Criaçao do form e classes padroes da aula

---
+++ WinFormsApp/WinFormsApp/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace WinFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            hopeForm1 = new ReaLTaiizor.Forms.HopeForm();
+            hopeButton1 = new ReaLTaiizor.Controls.HopeButton();
+            hopeButton2 = new ReaLTaiizor.Controls.HopeButton();
+            hopeTextBox1 = new ReaLTaiizor.Controls.HopeTextBox();
+            hopeTextBox2 = new ReaLTaiizor.Controls.HopeTextBox();
+            hopeTextBox3 = new ReaLTaiizor.Controls.HopeTextBox();
+            bigLabel1 = new ReaLTaiizor.Controls.BigLabel();
+            SuspendLayout();
+            // 
+            // hopeForm1
+            // 
+            hopeForm1.ControlBoxColorH = Color.FromArgb(228, 231, 237);
+            hopeForm1.ControlBoxColorHC = Color.FromArgb(245, 108, 108);
+            hopeForm1.ControlBoxColorN = Color.White;
+            hopeForm1.Dock = DockStyle.Top;
+            hopeForm1.Font = new Font("Segoe UI", 12F);
+            hopeForm1.ForeColor = Color.FromArgb(242, 246, 252);
+            hopeForm1.Image = (Image)resources.GetObject("hopeForm1.Image");
+            hopeForm1.Location = new Point(0, 0);
+            hopeForm1.Name = "hopeForm1";
+            hopeForm1.Size = new Size(414, 40);
+            hopeForm1.TabIndex = 0;
+            hopeForm1.Text = "hopeForm1";
+            hopeForm1.ThemeColor = Color.FromArgb(92, 173, 255);
+            // 
+            // hopeButton1
+            // 
+            hopeButton1.BorderColor = Color.FromArgb(220, 223, 230);
+            hopeButton1.ButtonType = ReaLTaiizor.Util.HopeButtonType.Primary;
+            hopeButton1.DangerColor = Color.FromArgb(245, 108, 108);
+            hopeButton1.DefaultColor = Color.FromArgb(255, 255, 255);
+            hopeButton1.Font = new Font("Segoe UI", 12F);
+            hopeButton1.HoverTextColor = Color.FromArgb(48, 49, 51);
+            hopeButton1.InfoColor = Color.FromArgb(144, 147, 153);
+            hopeButton1.Location = new Point(43, 340);
+            hopeButton1.Name = "hopeButton1";
+            hopeButton1.PrimaryColor = Color.FromArgb(64, 158, 255);
+            hopeButton1.Size = new Size(152, 40);
+            hopeButton1.SuccessColor = Color.FromArgb(103, 194, 58);
+            hopeButton1.TabIndex = 1;
+            hopeButton1.Text = "Salvar";
+            hopeButton1.TextColor = Color.White;
+            hopeButton1.WarningColor = Color.FromArgb(230, 162, 60);
+            // 
+            // hopeButton2
+            // 
+            hopeButton2.BorderColor = Color.FromArgb(220, 223, 230);
+            hopeButton2.ButtonType = ReaLTaiizor.Util.HopeButtonType.Primary;
+            hopeButton2.DangerColor = Color.FromArgb(245, 108, 108);
+            hopeButton2.DefaultColor = Color.FromArgb(255, 255, 255);
+            hopeButton2.Font = new Font("Segoe UI", 12F);
+            hopeButton2.HoverTextColor = Color.FromArgb(48, 49, 51);
+            hopeButton2.InfoColor = Color.FromArgb(144, 147, 153);
+            hopeButton2.Location = new Point(220, 340);
+            hopeButton2.Name = "hopeButton2";
+            hopeButton2.PrimaryColor = Color.FromArgb(64, 158, 255);
+            hopeButton2.Size = new Size(149, 40);
+            hopeButton2.SuccessColor = Color.FromArgb(103, 194, 58);
+            hopeButton2.TabIndex = 2;
+            hopeButton2.Text = "Cancelar";
+            hopeButton2.TextColor = Color.White;
+            hopeButton2.WarningColor = Color.FromArgb(230, 162, 60);
+            // 
+            // hopeTextBox1
+            // 
+            hopeTextBox1.BackColor = Color.White;
+            hopeTextBox1.BaseColor = Color.FromArgb(44, 55, 66);
+            hopeTextBox1.BorderColorA = Color.FromArgb(64, 158, 255);
+            hopeTextBox1.BorderColorB = Color.FromArgb(220, 223, 230);
+            hopeTextBox1.Font = new Font("Segoe UI", 12F);
+            hopeTextBox1.ForeColor = Color.FromArgb(48, 49, 51);
+            hopeTextBox1.Hint = "";
+            hopeTextBox1.Location = new Point(43, 133);
+            hopeTextBox1.MaxLength = 32767;
+            hopeTextBox1.Multiline = false;
+            hopeTextBox1.Name = "hopeTextBox1";
+            hopeTextBox1.PasswordChar = '\0';
+            hopeTextBox1.ScrollBars = ScrollBars.None;
+            hopeTextBox1.SelectedText = "";
+            hopeTextBox1.SelectionLength = 0;
+            hopeTextBox1.SelectionStart = 0;
+            hopeTextBox1.Size = new Size(326, 38);
+            hopeTextBox1.TabIndex = 3;
+            hopeTextBox1.TabStop = false;
+            hopeTextBox1.Text = "Nome";
+            hopeTextBox1.UseSystemPasswordChar = false;
+            // 
+            // hopeTextBox2
+            // 
+            hopeTextBox2.BackColor = Color.White;
+            hopeTextBox2.BaseColor = Color.FromArgb(44, 55, 66);
+            hopeTextBox2.BorderColorA = Color.FromArgb(64, 158, 255);
+            hopeTextBox2.BorderColorB = Color.FromArgb(220, 223, 230);
+            hopeTextBox2.Font = new Font("Segoe UI", 12F);
+            hopeTextBox2.ForeColor = Color.FromArgb(48, 49, 51);
+            hopeTextBox2.Hint = "";
+            hopeTextBox2.Location = new Point(43, 198);
+            hopeTextBox2.MaxLength = 32767;
+            hopeTextBox2.Multiline = false;
+            hopeTextBox2.Name = "hopeTextBox2";
+            hopeTextBox2.PasswordChar = '\0';
+            hopeTextBox2.ScrollBars = ScrollBars.None;
+            hopeTextBox2.SelectedText = "";
+            hopeTextBox2.SelectionLength = 0;
+            hopeTextBox2.SelectionStart = 0;
+            hopeTextBox2.Size = new Size(326, 38);
+            hopeTextBox2.TabIndex = 4;
+            hopeTextBox2.TabStop = false;
+            hopeTextBox2.Text = "Email";
+            hopeTextBox2.UseSystemPasswordChar = false;
+            // 
+            // hopeTextBox3
+            // 
+            hopeTextBox3.BackColor = Color.White;
+            hopeTextBox3.BaseColor = Color.FromArgb(44, 55, 66);
+            hopeTextBox3.BorderColorA = Color.FromArgb(64, 158, 255);
+            hopeTextBox3.BorderColorB = Color.FromArgb(220, 223, 230);
+            hopeTextBox3.Font = new Font("Segoe UI", 12F);
+            hopeTextBox3.ForeColor = Color.FromArgb(48, 49, 51);
+            hopeTextBox3.Hint = "";
+            hopeTextBox3.Location = new Point(43, 262);
+            hopeTextBox3.MaxLength = 32767;
+            hopeTextBox3.Multiline = false;
+            hopeTextBox3.Name = "hopeTextBox3";
+            hopeTextBox3.PasswordChar = '\0';
+            hopeTextBox3.ScrollBars = ScrollBars.None;
+            hopeTextBox3.SelectedText = "";
+            hopeTextBox3.SelectionLength = 0;
+            hopeTextBox3.SelectionStart = 0;
+            hopeTextBox3.Size = new Size(326, 38);
+            hopeTextBox3.TabIndex = 5;
+            hopeTextBox3.TabStop = false;
+            hopeTextBox3.Text = "Senha";
+            hopeTextBox3.UseSystemPasswordChar = false;
+            // 
+            // bigLabel1
+            // 
+            bigLabel1.AutoSize = true;
+            bigLabel1.BackColor = Color.Transparent;
+            bigLabel1.Font = new Font("Segoe UI", 25F);
+            bigLabel1.ForeColor = Color.FromArgb(80, 80, 80);
+            bigLabel1.Location = new Point(130, 68);
+            bigLabel1.Name = "bigLabel1";
+            bigLabel1.Size = new Size(153, 46);
+            bigLabel1.TabIndex = 6;
+            bigLabel1.Text = "Cadastro";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(414, 450);
+            Controls.Add(bigLabel1);
+            Controls.Add(hopeTextBox3);
+            Controls.Add(hopeTextBox2);
+            Controls.Add(hopeTextBox1);
+            Controls.Add(hopeButton2);
+            Controls.Add(hopeButton1);
+            Controls.Add(hopeForm1);
+            FormBorderStyle = FormBorderStyle.None;
+            MaximumSize = new Size(1600, 860);
+            MinimumSize = new Size(190, 40);
+            Name = "Form1";
+            ShowIcon = false;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "themeForm1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ReaLTaiizor.Forms.HopeForm hopeForm1;
+        private ReaLTaiizor.Controls.HopeButton hopeButton1;
+        private ReaLTaiizor.Controls.HopeButton hopeButton2;
+        private ReaLTaiizor.Controls.HopeTextBox hopeTextBox1;
+        private ReaLTaiizor.Controls.HopeTextBox hopeTextBox2;
+        private ReaLTaiizor.Controls.HopeTextBox hopeTextBox3;
+        private ReaLTaiizor.Controls.BigLabel bigLabel1;
+    }
+}
---
+++ WinFormsApp/WinFormsApp/Form1.cs
@@ -0,0 +1,11 @@
+namespace WinFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
